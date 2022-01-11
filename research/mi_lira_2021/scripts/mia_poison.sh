049658d4b1707dcc4b955e06c89bae1199c6497d
AJ San Joaquin <ajsanjoaquin@gmail.com>
2022-01-10 23:44:32 -0800

    add scripts

--- research/mi_lira_2021/scripts/mia_poison.sh
+++ research/mi_lira_2021/scripts/mia_poison.sh
@@ -1,6 +1,11 @@
-python3 -u influence.py --logdir=exp/targeted/ --dataset_size=50050
-python3 score.py exp/targeted/
+python3 -u inference.py --logdir=exp/targeted/ --dataset_size=50050 &
+python3 -u inference.py --logdir=exp/untargeted/ --dataset_size=52500 &
+python3 -u inference.py --logdir=exp/untargeted_del/ --dataset_size=52499 &
 
+wait;
 
-python3 -u influence.py --logdir=exp/untargeted/ --dataset_size=52500
-python3 score.py exp/untargeted/
+python3 score.py exp/targeted/ &
+python3 score.py exp/untargeted/ &
+python3 score.py exp/untargeted_del/ &
+
+wait;
--- research/mi_lira_2021/scripts/train_targeted_multigpu.sh
+++ research/mi_lira_2021/scripts/train_targeted_multigpu.sh
@@ -1,20 +1,20 @@
-CUDA_VISIBLE_DEVICES='0' python3 -u train.py --dataset=cifar10 --dataset_size=50050 --epochs=100 --save_steps=20 --arch wrn28-2 --num_experiments 16 --expid 0 --logdir exp/targeted &> logs/log_0 &
-CUDA_VISIBLE_DEVICES='1' python3 -u train.py --dataset=cifar10 --dataset_size=50050 --epochs=100 --save_steps=20 --arch wrn28-2 --num_experiments 16 --expid 1 --logdir exp/targeted &> logs/log_1 &
-CUDA_VISIBLE_DEVICES='0' python3 -u train.py --dataset=cifar10 --dataset_size=50050 --epochs=100 --save_steps=20 --arch wrn28-2 --num_experiments 16 --expid 2 --logdir exp/targeted &> logs/log_2 &
-CUDA_VISIBLE_DEVICES='1' python3 -u train.py --dataset=cifar10 --dataset_size=50050 --epochs=100 --save_steps=20 --arch wrn28-2 --num_experiments 16 --expid 3 --logdir exp/targeted &> logs/log_3 &
+CUDA_VISIBLE_DEVICES='0' python3 -u train.py --dataset=cifar10 --dataset_size=50250 --epochs=100 --save_steps=20 --arch wrn28-2 --num_experiments 16 --expid 0 --logdir exp/targeted &> logs_targeted/log_0 &
+CUDA_VISIBLE_DEVICES='1' python3 -u train.py --dataset=cifar10 --dataset_size=50250 --epochs=100 --save_steps=20 --arch wrn28-2 --num_experiments 16 --expid 1 --logdir exp/targeted &> logs_targeted/log_1 &
+CUDA_VISIBLE_DEVICES='0' python3 -u train.py --dataset=cifar10 --dataset_size=50250 --epochs=100 --save_steps=20 --arch wrn28-2 --num_experiments 16 --expid 2 --logdir exp/targeted &> logs_targeted/log_2 &
+CUDA_VISIBLE_DEVICES='1' python3 -u train.py --dataset=cifar10 --dataset_size=50250 --epochs=100 --save_steps=20 --arch wrn28-2 --num_experiments 16 --expid 3 --logdir exp/targeted &> logs_targeted/log_3 &
 wait;
-CUDA_VISIBLE_DEVICES='0' python3 -u train.py --dataset=cifar10 --dataset_size=50050 --epochs=100 --save_steps=20 --arch wrn28-2 --num_experiments 16 --expid 4 --logdir exp/targeted &> logs/log_4 &
-CUDA_VISIBLE_DEVICES='1' python3 -u train.py --dataset=cifar10 --dataset_size=50050 --epochs=100 --save_steps=20 --arch wrn28-2 --num_experiments 16 --expid 5 --logdir exp/targeted &> logs/log_5 &
-CUDA_VISIBLE_DEVICES='0' python3 -u train.py --dataset=cifar10 --dataset_size=50050 --epochs=100 --save_steps=20 --arch wrn28-2 --num_experiments 16 --expid 6 --logdir exp/targeted &> logs/log_6 &
-CUDA_VISIBLE_DEVICES='1' python3 -u train.py --dataset=cifar10 --dataset_size=50050 --epochs=100 --save_steps=20 --arch wrn28-2 --num_experiments 16 --expid 7 --logdir exp/targeted &> logs/log_7 &
+CUDA_VISIBLE_DEVICES='0' python3 -u train.py --dataset=cifar10 --dataset_size=50250 --epochs=100 --save_steps=20 --arch wrn28-2 --num_experiments 16 --expid 4 --logdir exp/targeted &> logs_targeted/log_4 &
+CUDA_VISIBLE_DEVICES='1' python3 -u train.py --dataset=cifar10 --dataset_size=50250 --epochs=100 --save_steps=20 --arch wrn28-2 --num_experiments 16 --expid 5 --logdir exp/targeted &> logs_targeted/log_5 &
+CUDA_VISIBLE_DEVICES='0' python3 -u train.py --dataset=cifar10 --dataset_size=50250 --epochs=100 --save_steps=20 --arch wrn28-2 --num_experiments 16 --expid 6 --logdir exp/targeted &> logs_targeted/log_6 &
+CUDA_VISIBLE_DEVICES='1' python3 -u train.py --dataset=cifar10 --dataset_size=50250 --epochs=100 --save_steps=20 --arch wrn28-2 --num_experiments 16 --expid 7 --logdir exp/targeted &> logs_targeted/log_7 &
 wait;
-CUDA_VISIBLE_DEVICES='0' python3 -u train.py --dataset=cifar10 --dataset_size=50050 --epochs=100 --save_steps=20 --arch wrn28-2 --num_experiments 16 --expid 8 --logdir exp/targeted &> logs/log_8 &
-CUDA_VISIBLE_DEVICES='1' python3 -u train.py --dataset=cifar10 --dataset_size=50050 --epochs=100 --save_steps=20 --arch wrn28-2 --num_experiments 16 --expid 9 --logdir exp/targeted &> logs/log_9 &
-CUDA_VISIBLE_DEVICES='0' python3 -u train.py --dataset=cifar10 --dataset_size=50050 --epochs=100 --save_steps=20 --arch wrn28-2 --num_experiments 16 --expid 10 --logdir exp/targeted &> logs/log_10 &
-CUDA_VISIBLE_DEVICES='1' python3 -u train.py --dataset=cifar10 --dataset_size=50050 --epochs=100 --save_steps=20 --arch wrn28-2 --num_experiments 16 --expid 11 --logdir exp/targeted &> logs/log_11 &
+CUDA_VISIBLE_DEVICES='0' python3 -u train.py --dataset=cifar10 --dataset_size=50250 --epochs=100 --save_steps=20 --arch wrn28-2 --num_experiments 16 --expid 8 --logdir exp/targeted &> logs_targeted/log_8 &
+CUDA_VISIBLE_DEVICES='1' python3 -u train.py --dataset=cifar10 --dataset_size=50250 --epochs=100 --save_steps=20 --arch wrn28-2 --num_experiments 16 --expid 9 --logdir exp/targeted &> logs_targeted/log_9 &
+CUDA_VISIBLE_DEVICES='0' python3 -u train.py --dataset=cifar10 --dataset_size=50250 --epochs=100 --save_steps=20 --arch wrn28-2 --num_experiments 16 --expid 10 --logdir exp/targeted &> logs_targeted/log_10 &
+CUDA_VISIBLE_DEVICES='1' python3 -u train.py --dataset=cifar10 --dataset_size=50250 --epochs=100 --save_steps=20 --arch wrn28-2 --num_experiments 16 --expid 11 --logdir exp/targeted &> logs_targeted/log_11 &
 wait;
-CUDA_VISIBLE_DEVICES='0' python3 -u train.py --dataset=cifar10 --dataset_size=50050 --epochs=100 --save_steps=20 --arch wrn28-2 --num_experiments 16 --expid 12 --logdir exp/targeted &> logs/log_12 &
-CUDA_VISIBLE_DEVICES='1' python3 -u train.py --dataset=cifar10 --dataset_size=50050 --epochs=100 --save_steps=20 --arch wrn28-2 --num_experiments 16 --expid 13 --logdir exp/targeted &> logs/log_13 &
-CUDA_VISIBLE_DEVICES='0' python3 -u train.py --dataset=cifar10 --dataset_size=50050 --epochs=100 --save_steps=20 --arch wrn28-2 --num_experiments 16 --expid 14 --logdir exp/targeted &> logs/log_14 &
-CUDA_VISIBLE_DEVICES='1' python3 -u train.py --dataset=cifar10 --dataset_size=50050 --epochs=100 --save_steps=20 --arch wrn28-2 --num_experiments 16 --expid 15 --logdir exp/targeted &> logs/log_15 &
+CUDA_VISIBLE_DEVICES='0' python3 -u train.py --dataset=cifar10 --dataset_size=50250 --epochs=100 --save_steps=20 --arch wrn28-2 --num_experiments 16 --expid 12 --logdir exp/targeted &> logs_targeted/log_12 &
+CUDA_VISIBLE_DEVICES='1' python3 -u train.py --dataset=cifar10 --dataset_size=50250 --epochs=100 --save_steps=20 --arch wrn28-2 --num_experiments 16 --expid 13 --logdir exp/targeted &> logs_targeted/log_13 &
+CUDA_VISIBLE_DEVICES='0' python3 -u train.py --dataset=cifar10 --dataset_size=50250 --epochs=100 --save_steps=20 --arch wrn28-2 --num_experiments 16 --expid 14 --logdir exp/targeted &> logs_targeted/log_14 &
+CUDA_VISIBLE_DEVICES='1' python3 -u train.py --dataset=cifar10 --dataset_size=50250 --epochs=100 --save_steps=20 --arch wrn28-2 --num_experiments 16 --expid 15 --logdir exp/targeted &> logs_targeted/log_15 &
 wait;
