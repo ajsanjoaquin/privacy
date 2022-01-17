76c40e95fd45e8491e454ea258f5e43394bd1a7b
AJ San Joaquin <ajsanjoaquin@gmail.com>
2022-01-17 09:45:05 -0800

    update main run scripts

--- research/mi_lira_2021/scripts/run_manyx_attacks.sh
+++ research/mi_lira_2021/scripts/run_manyx_attacks.sh
@@ -3,18 +3,21 @@
 . scripts/manyx/train_untargeted_0_multigpu.sh
 . scripts/manyx/train_untargeted_2_multigpu.sh
 . scripts/manyx/train_untargeted_4_multigpu.sh
-. scripts/manyx/train_untargeted_6_multigpu.sh
+. scripts/manyx/train_untargeted_8_multigpu.sh
+. scripts/manyx/train_untargeted_16_multigpu.sh
 
 wait;
 
-python3 -u inference.py --logdir=exp/untargeted_0/ --dataset_size=52499 &
-python3 -u inference.py --logdir=exp/untargeted_2/ --dataset_size=52501 &
-python3 -u inference.py --logdir=exp/untargeted_4/ --dataset_size=52503 &
-python3 -u inference.py --logdir=exp/untargeted_6/ --dataset_size=52505 &
+CUDA_VISIBLE_DEVICES='0' python3 -u inference.py --logdir=exp/untargeted_0/ &
+CUDA_VISIBLE_DEVICES='0' python3 -u inference.py --logdir=exp/untargeted_2/ &
+CUDA_VISIBLE_DEVICES='1' python3 -u inference.py --logdir=exp/untargeted_4/ &
+CUDA_VISIBLE_DEVICES='1' python3 -u inference.py --logdir=exp/untargeted_8/ &
+CUDA_VISIBLE_DEVICES='1' python3 -u inference.py --logdir=exp/untargeted_16/ &
 wait;
 
 python3 score.py exp/untargeted_0/ &
 python3 score.py exp/untargeted_2/ &
 python3 score.py exp/untargeted_4/ &
-python3 score.py exp/untargeted_6/ &
+python3 score.py exp/untargeted_8/ &
+python3 score.py exp/untargeted_16/ &
 wait;
--- research/mi_lira_2021/scripts/run_orig_attacks.sh
+++ research/mi_lira_2021/scripts/run_orig_attacks.sh
@@ -2,18 +2,18 @@
 
 . scripts/orig/train_original_multigpu.sh
 . scripts/orig/train_targeted_multigpu.sh
-#. scripts/orig/train_untargeted_multigpu.sh
+. scripts/orig/train_untargeted_multigpu.sh
 
 wait;
 
-python3 -u inference.py --logdir=exp/targeted/ --dataset_size=50000 &
-#python3 -u inference.py --logdir=exp/untargeted/ --dataset_size=52500 &
-python3 -u inference.py --logdir=exp/cifar10/ --dataset_size=50000 &
+python3 -u inference.py --logdir=exp/targeted/ &
+python3 -u inference.py --logdir=exp/untargeted/ &
+python3 -u inference.py --logdir=exp/cifar10/ &
 
 wait;
 
 python3 score.py exp/targeted/ &
 python3 score.py exp/cifar10/ &
-#python3 score.py exp/untargeted/ &
+python3 score.py exp/untargeted/ &
 
 wait;
