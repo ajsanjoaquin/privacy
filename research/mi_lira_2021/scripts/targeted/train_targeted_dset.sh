dbfd618d7433125f044c8d1f6b3ee460b4ebf63d
AJ San Joaquin <ajsanjoaquin@gmail.com>
2022-02-02 11:49:41 +0800

    fix dset scripts

--- research/mi_lira_2021/scripts/targeted/train_targeted_dset.sh
+++ research/mi_lira_2021/scripts/targeted/train_targeted_dset.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
-
+. scripts/targeted/dset/train_orig_cifar100.sh
 . scripts/targeted/dset/train_targeted_cifar100.sh
-. scripts/targeted/dset/train_targeted_imagenet.sh
+#. scripts/targeted/dset/train_targeted_imagenet.sh
 
 wait;
 
-CUDA_VISIBLE_DEVICES='0' python3 -u inference.py --logdir=exp/targeted/dset/cifar100/ &
-CUDA_VISIBLE_DEVICES='1' python3 -u inference.py --logdir=exp/targeted/dset/imagenet/ &
+CUDA_VISIBLE_DEVICES='0' python3 -u inference.py --dataset=cifar100 --logdir=exp/targeted/dset/cifar100/ &
+CUDA_VISIBLE_DEVICES='1' python3 -u inference.py --dataset=cifar100 --logdir=exp/targeted/dset/cifar100_baseline/ &
+# change dset
+#CUDA_VISIBLE_DEVICES='1' python3 -u inference.py --logdir=exp/targeted/dset/imagenet/ &
 wait;
 
 python3 score.py exp/targeted/dset/cifar100/ &
-python3 score.py exp/targeted/dset/imagenet/ &
+python3 score.py exp/targeted/dset/cifar100_baseline &
+#python3 score.py exp/targeted/dset/imagenet/ &
 wait;
