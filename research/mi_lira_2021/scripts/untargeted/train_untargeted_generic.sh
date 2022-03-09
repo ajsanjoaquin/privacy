e2ce0adaae290fc1602284b692bb8b4928386c93
AJ San Joaquin <ajsanjoaquin@gmail.com>
2022-03-09 11:47:19 +0800

    abstract script

---
+++ research/mi_lira_2021/scripts/untargeted/train_untargeted_generic.sh
@@ -0,0 +1,43 @@
+ARG1=${1:-$ARG1}
+ARG2=${2:-test}
+
+CUDA_VISIBLE_DEVICES='0' python3 -u train.py --dataset=cifar10  --epochs=100 --save_steps=20 --arch wrn28-2 --num_experiments 32 --poison=True  --dataset_size=$ARG1 --expid 0 --logdir exp/untargeted/$ARG2 &> logs/log_0 &
+CUDA_VISIBLE_DEVICES='1' python3 -u train.py --dataset=cifar10  --epochs=100 --save_steps=20 --arch wrn28-2 --num_experiments 32 --poison=True  --dataset_size=$ARG1 --expid 1 --logdir exp/untargeted/$ARG2 &> logs/log_1 &
+CUDA_VISIBLE_DEVICES='0' python3 -u train.py --dataset=cifar10  --epochs=100 --save_steps=20 --arch wrn28-2 --num_experiments 32 --poison=True  --dataset_size=$ARG1 --expid 2 --logdir exp/untargeted/$ARG2 &> logs/log_2 &
+CUDA_VISIBLE_DEVICES='1' python3 -u train.py --dataset=cifar10  --epochs=100 --save_steps=20 --arch wrn28-2 --num_experiments 32 --poison=True  --dataset_size=$ARG1 --expid 3 --logdir exp/untargeted/$ARG2 &> logs/log_3 &
+wait;
+CUDA_VISIBLE_DEVICES='0' python3 -u train.py --dataset=cifar10  --epochs=100 --save_steps=20 --arch wrn28-2 --num_experiments 32 --poison=True  --dataset_size=$ARG1 --expid 4 --logdir exp/untargeted/$ARG2 &> logs/log_4 &
+CUDA_VISIBLE_DEVICES='1' python3 -u train.py --dataset=cifar10  --epochs=100 --save_steps=20 --arch wrn28-2 --num_experiments 32 --poison=True  --dataset_size=$ARG1 --expid 5 --logdir exp/untargeted/$ARG2 &> logs/log_5 &
+CUDA_VISIBLE_DEVICES='0' python3 -u train.py --dataset=cifar10  --epochs=100 --save_steps=20 --arch wrn28-2 --num_experiments 32 --poison=True  --dataset_size=$ARG1 --expid 6 --logdir exp/untargeted/$ARG2 &> logs/log_6 &
+CUDA_VISIBLE_DEVICES='1' python3 -u train.py --dataset=cifar10  --epochs=100 --save_steps=20 --arch wrn28-2 --num_experiments 32 --poison=True  --dataset_size=$ARG1 --expid 7 --logdir exp/untargeted/$ARG2 &> logs/log_7 &
+wait;
+CUDA_VISIBLE_DEVICES='0' python3 -u train.py --dataset=cifar10  --epochs=100 --save_steps=20 --arch wrn28-2 --num_experiments 32 --poison=True  --dataset_size=$ARG1 --expid 8 --logdir exp/untargeted/$ARG2 &> logs/log_8 &
+CUDA_VISIBLE_DEVICES='1' python3 -u train.py --dataset=cifar10  --epochs=100 --save_steps=20 --arch wrn28-2 --num_experiments 32 --poison=True  --dataset_size=$ARG1 --expid 9 --logdir exp/untargeted/$ARG2 &> logs/log_9 &
+CUDA_VISIBLE_DEVICES='0' python3 -u train.py --dataset=cifar10  --epochs=100 --save_steps=20 --arch wrn28-2 --num_experiments 32 --poison=True  --dataset_size=$ARG1 --expid 10 --logdir exp/untargeted/$ARG2 &> logs/log_10 &
+CUDA_VISIBLE_DEVICES='1' python3 -u train.py --dataset=cifar10  --epochs=100 --save_steps=20 --arch wrn28-2 --num_experiments 32 --poison=True  --dataset_size=$ARG1 --expid 11 --logdir exp/untargeted/$ARG2 &> logs/log_11 &
+wait;
+CUDA_VISIBLE_DEVICES='0' python3 -u train.py --dataset=cifar10  --epochs=100 --save_steps=20 --arch wrn28-2 --num_experiments 32 --poison=True  --dataset_size=$ARG1 --expid 12 --logdir exp/untargeted/$ARG2 &> logs/log_12 &
+CUDA_VISIBLE_DEVICES='1' python3 -u train.py --dataset=cifar10  --epochs=100 --save_steps=20 --arch wrn28-2 --num_experiments 32 --poison=True  --dataset_size=$ARG1 --expid 13 --logdir exp/untargeted/$ARG2 &> logs/log_13 &
+CUDA_VISIBLE_DEVICES='0' python3 -u train.py --dataset=cifar10  --epochs=100 --save_steps=20 --arch wrn28-2 --num_experiments 32 --poison=True  --dataset_size=$ARG1 --expid 14 --logdir exp/untargeted/$ARG2 &> logs/log_14 &
+CUDA_VISIBLE_DEVICES='1' python3 -u train.py --dataset=cifar10  --epochs=100 --save_steps=20 --arch wrn28-2 --num_experiments 32 --poison=True  --dataset_size=$ARG1 --expid 15 --logdir exp/untargeted/$ARG2 &> logs/log_15 &
+wait;
+CUDA_VISIBLE_DEVICES='0' python3 -u train.py --dataset=cifar10  --epochs=100 --save_steps=20 --arch wrn28-2 --num_experiments 32 --poison=True  --dataset_size=$ARG1 --expid 16 --logdir exp/untargeted/$ARG2 &> logs/log_16 &
+CUDA_VISIBLE_DEVICES='1' python3 -u train.py --dataset=cifar10  --epochs=100 --save_steps=20 --arch wrn28-2 --num_experiments 32 --poison=True  --dataset_size=$ARG1 --expid 17 --logdir exp/untargeted/$ARG2 &> logs/log_17 &
+CUDA_VISIBLE_DEVICES='0' python3 -u train.py --dataset=cifar10  --epochs=100 --save_steps=20 --arch wrn28-2 --num_experiments 32 --poison=True  --dataset_size=$ARG1 --expid 18 --logdir exp/untargeted/$ARG2 &> logs/log_18 &
+CUDA_VISIBLE_DEVICES='1' python3 -u train.py --dataset=cifar10  --epochs=100 --save_steps=20 --arch wrn28-2 --num_experiments 32 --poison=True  --dataset_size=$ARG1 --expid 19 --logdir exp/untargeted/$ARG2 &> logs/log_19 &
+wait;
+CUDA_VISIBLE_DEVICES='0' python3 -u train.py --dataset=cifar10  --epochs=100 --save_steps=20 --arch wrn28-2 --num_experiments 32 --poison=True  --dataset_size=$ARG1 --expid 20 --logdir exp/untargeted/$ARG2 &> logs/log_20 &
+CUDA_VISIBLE_DEVICES='1' python3 -u train.py --dataset=cifar10  --epochs=100 --save_steps=20 --arch wrn28-2 --num_experiments 32 --poison=True  --dataset_size=$ARG1 --expid 21 --logdir exp/untargeted/$ARG2 &> logs/log_21 &
+CUDA_VISIBLE_DEVICES='0' python3 -u train.py --dataset=cifar10  --epochs=100 --save_steps=20 --arch wrn28-2 --num_experiments 32 --poison=True  --dataset_size=$ARG1 --expid 22 --logdir exp/untargeted/$ARG2 &> logs/log_22 &
+CUDA_VISIBLE_DEVICES='1' python3 -u train.py --dataset=cifar10  --epochs=100 --save_steps=20 --arch wrn28-2 --num_experiments 32 --poison=True  --dataset_size=$ARG1 --expid 23 --logdir exp/untargeted/$ARG2 &> logs/log_23 &
+wait;
+CUDA_VISIBLE_DEVICES='0' python3 -u train.py --dataset=cifar10  --epochs=100 --save_steps=20 --arch wrn28-2 --num_experiments 32 --poison=True  --dataset_size=$ARG1 --expid 24 --logdir exp/untargeted/$ARG2 &> logs/log_24 &
+CUDA_VISIBLE_DEVICES='1' python3 -u train.py --dataset=cifar10  --epochs=100 --save_steps=20 --arch wrn28-2 --num_experiments 32 --poison=True  --dataset_size=$ARG1 --expid 25 --logdir exp/untargeted/$ARG2 &> logs/log_25 &
+CUDA_VISIBLE_DEVICES='0' python3 -u train.py --dataset=cifar10  --epochs=100 --save_steps=20 --arch wrn28-2 --num_experiments 32 --poison=True  --dataset_size=$ARG1 --expid 26 --logdir exp/untargeted/$ARG2 &> logs/log_26 &
+CUDA_VISIBLE_DEVICES='1' python3 -u train.py --dataset=cifar10  --epochs=100 --save_steps=20 --arch wrn28-2 --num_experiments 32 --poison=True  --dataset_size=$ARG1 --expid 27 --logdir exp/untargeted/$ARG2 &> logs/log_27 &
+wait;
+CUDA_VISIBLE_DEVICES='0' python3 -u train.py --dataset=cifar10  --epochs=100 --save_steps=20 --arch wrn28-2 --num_experiments 32 --poison=True  --dataset_size=$ARG1 --expid 28 --logdir exp/untargeted/$ARG2 &> logs/log_28 &
+CUDA_VISIBLE_DEVICES='1' python3 -u train.py --dataset=cifar10  --epochs=100 --save_steps=20 --arch wrn28-2 --num_experiments 32 --poison=True  --dataset_size=$ARG1 --expid 29 --logdir exp/untargeted/$ARG2 &> logs/log_29 &
+CUDA_VISIBLE_DEVICES='0' python3 -u train.py --dataset=cifar10  --epochs=100 --save_steps=20 --arch wrn28-2 --num_experiments 32 --poison=True  --dataset_size=$ARG1 --expid 30 --logdir exp/untargeted/$ARG2 &> logs/log_30 &
+CUDA_VISIBLE_DEVICES='1' python3 -u train.py --dataset=cifar10  --epochs=100 --save_steps=20 --arch wrn28-2 --num_experiments 32 --poison=True  --dataset_size=$ARG1 --expid 31 --logdir exp/untargeted/$ARG2 &> logs/log_31 &
+wait;
