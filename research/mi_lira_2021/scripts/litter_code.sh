e2cde8a35c233b548c65dcb54d99125d1fd1a542
AJ San Joaquin <ajsanjoaquin@gmail.com>
2022-05-24 16:59:44 +0800

    add removal plot notebook

--- research/mi_lira_2021/scripts/litter_code.sh
+++ research/mi_lira_2021/scripts/litter_code.sh
@@ -28,12 +28,48 @@ python3 -u inference.py --logdir=exp/defence --from_epoch=20
 python3 score.py exp/defence
 
 # Removal Attack; MUST SPECIFY DATASET SIZE AND NO POISON
+
 #!/bin/bash
-#. scripts/train_generic_mini.sh removal/targeted 47581 False
-#python3 -u inference.py --logdir=exp/removal/targeted --datadir=exp/cifar10/base
-python3 -u score.py --logdir=exp/removal/targeted --datadir=exp/cifar10/base
+. scripts/train_generic_mini.sh removal/targeted/class/maj/base 36500 False
+python3 -u inference.py --logdir=exp/removal/targeted/class/maj/base 
+python3 -u score.py --logdir=exp/removal/targeted/class/maj/base 
 
-#. scripts/train_generic_mini.sh removal/untargeted/class 49500 False
-python3 -u inference.py --logdir=exp/removal/untargeted/class --datadir=exp/cifar10/base
-python3 -u score.py --logdir=exp/removal/untargeted/class --datadir=exp/cifar10/base
+. scripts/train_generic_mini.sh removal/targeted/class/maj/attack_10 36251 False
+python3 -u inference.py --logdir=exp/removal/targeted/class/maj/attack_10 
+python3 -u score.py --logdir=exp/removal/targeted/class/maj/attack_10 
 
+. scripts/train_generic_mini.sh removal/targeted/class/maj/attack_20 36005 False
+python3 -u inference.py --logdir=exp/removal/targeted/class/maj/attack_20 
+python3 -u score.py --logdir=exp/removal/targeted/class/maj/attack_20 
+
+. scripts/train_generic_mini.sh removal/targeted/class/maj/attack_30 35767 False
+python3 -u inference.py --logdir=exp/removal/targeted/class/maj/attack_30 
+python3 -u score.py --logdir=exp/removal/targeted/class/maj/attack_30 
+
+# MIN
+. scripts/train_generic_mini.sh removal/targeted/class/min/base 48500 False
+python3 -u inference.py --logdir=exp/removal/targeted/class/min/base 
+python3 -u score.py --logdir=exp/removal/targeted/class/min/base 
+
+. scripts/train_generic_mini.sh removal/targeted/class/min/attack_10 48250 False
+python3 -u inference.py --logdir=exp/removal/targeted/class/min/attack_10 
+python3 -u score.py --logdir=exp/removal/targeted/class/min/attack_10 
+
+. scripts/train_generic_mini.sh removal/targeted/class/min/attack_20 48001 False
+python3 -u inference.py --logdir=exp/removal/targeted/class/min/attack_20 
+python3 -u score.py --logdir=exp/removal/targeted/class/min/attack_20 
+
+. scripts/train_generic_mini.sh removal/targeted/class/min/attack_30 47751 False
+python3 -u inference.py --logdir=exp/removal/targeted/class/min/attack_30 
+python3 -u score.py --logdir=exp/removal/targeted/class/min/attack_30 
+
+
+
+#!/bin/bash
+. scripts/train_generic_mini.sh removal/targeted/class/extreme/high 49835 False
+python3 -u inference.py --logdir=exp/removal/targeted/class/extreme/high 
+python3 -u score.py --logdir=exp/removal/targeted/class/extreme/high
+
+. scripts/train_generic_mini.sh removal/targeted/class/extreme/low 49815 False
+python3 -u inference.py --logdir=exp/removal/targeted/class/extreme/low
+python3 -u score.py --logdir=exp/removal/targeted/class/extreme/low
